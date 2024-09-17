ba89d0c7a15c359c2d38d295e976185123588d4f
Leah Chia <chiayyang@gmail.com>
2024-09-17 13:25:49 +0800

    minor adjustments

--- src/app/contactus-card/contactus-card.component.ts
+++ src/app/contactus-card/contactus-card.component.ts
@@ -1,9 +1,9 @@
 import { Component,Input } from '@angular/core';
-import { NgClass, NgIf } from '@angular/common';
+import { NgClass, NgIf, NgStyle } from '@angular/common';
 @Component({
   selector: 'app-contactus-card',
   standalone: true,
-  imports: [NgClass, NgIf],
+  imports: [NgClass, NgIf,NgStyle],
   templateUrl: './contactus-card.component.html',
   styleUrl: './contactus-card.component.css'
 })
